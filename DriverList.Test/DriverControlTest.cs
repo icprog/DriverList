4e7d3a05f0aea9ed5aa1f038f8bfda8ce2eef8c7
IlyaChernov <ilja.chernov@gmail.com>
2018-03-25 23:41:03 +0300

    Added Moq test for DriverControl class

--- DriverList.Test/DriverControlTest.cs
+++ DriverList.Test/DriverControlTest.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using HardwareHelperLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace DriverList.Test
 {
     [TestClass]
     public class DriverControlTest
     {
+        private List<DEVICE_INFO> testDevices = new List<DEVICE_INFO>();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {            
+            testDevices.Add(new DEVICE_INFO { hardwareId = "TestID" });
+            testDevices.Add(new DEVICE_INFO { hardwareId = "TestID" });
+            testDevices.Add(new DEVICE_INFO { hardwareId = "TestID2" });
+        }
+
         [TestMethod]
         public void StopDevicesByIdTest()
         {
+            Mock<IDriverProvider> mock = new Mock<IDriverProvider>();
+            mock.Setup(oper => oper.GetDriverList()).Returns(testDevices);
+            mock.Setup(oper => oper.StopDevice(It.IsAny<DEVICE_INFO>()));
 
+            DriverControl dc = new DriverControl(mock.Object);
+
+            dc.StopDeviceByID("TestID");
+
+            mock.Verify(mck => mck.GetDriverList(), Times.Once());
+
+            mock.Verify(mck => mck.StopDevice(It.IsAny<DEVICE_INFO>()), Times.Exactly(2));
         }
     }
 }
